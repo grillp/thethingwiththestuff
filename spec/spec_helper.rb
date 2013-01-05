cd8934dad76863409fadb2d3524d883c3ff68f36
Gil <gil@cancas.com>
2013-01-05 14:44:15 +1100

    changed specherlp to load all files in src dir

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,1 @@
-require_relative "../src/point_of_sale_terminal.rb"
-require_relative "../src/price_point_helper.rb"
-require_relative "../src/calculator/unit_price_calculator.rb"
-require_relative "../src/calculator/volume_price_calculator.rb"
+Dir['src/**/*.rb'].each{|f| p f;require_relative "../#{f}" }
